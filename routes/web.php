91516d7c28fb7ae6d23df0488d0a2bb2d31aa44f
eslamandroid12345 <eslamemo457@gmail.com>
2022-11-01 11:36:30 +0200

    update url web

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;//use package of lan
 */
 
 
-
+   //web application url
     define('Max',1);
     define('Search',8);
     define('EVENT',1);

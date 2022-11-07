cbfa56039b59a8b354ba6c5521d969956fc9e496
eslamandroid12345 <eslamemo457@gmail.com>
2022-11-07 15:24:31 +0200

    update command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('hotels:expires')->monthly();
+        $schedule->command('hotels:expires')->everyMinute();
     }
 
 
